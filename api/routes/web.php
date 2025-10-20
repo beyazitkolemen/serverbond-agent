13a434f20a95d9bc966d86482bd63599ccf86707
Beyazıt Kölemen <bkolemen@gmail.com>
2025-10-20 16:31:01 +0300

    .

--- api/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// Vue.js SPA - Tüm route'ları Vue Router'a yönlendir
-Route::get('/{any}', function () {
-    return view('app');
-})->where('any', '.*');
